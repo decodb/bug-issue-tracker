0dc252309ced0a2c234f7f59483995d6f07d1035
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-07-08 16:19:00 +0200

    Sign up page

--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  form = new FormGroup({
+    firstName : new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    lastName : new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    email : new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password : new FormControl('', {
+      validators : [Validators.required, Validators.minLength(6)]
+    }),
+    confirmPassword : new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  })
 
+  onSubmit() {
+    console.log(this.form.value)
+    this.form.reset();
+  }
 }
